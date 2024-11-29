c945f2c20b45b3cd070bee86ca0f81811afaf66e
luizpaulino <lp.luizpaulino@gmail.com>
2024-11-29 00:02:37 -0300

    adiciona scripts  e  mais prettier config

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,24 @@
 function Home() {
-    return (
-        <div>
-            <h1>Na, fiz isso aqui por você e pela nossa family</h1>
+  return (
+    <div>
+      <h1>Na, fiz isso aqui por você e pela nossa family</h1>
 
-            <p>
-                Esse é um desafio dos meus estudos onde eu tenho que
-                escrever algo pra alguém que é importante pra mim e 
-                disponibilizar isso de forma pública na internet pra 
-                conseguir mandar pra esse pessoa.
-            </p>
+      <p>
+        Esse é um desafio dos meus estudos onde eu tenho que escrever algo pra
+        alguém que é importante pra mim e disponibilizar isso de forma pública
+        na internet pra conseguir mandar pra esse pessoa.
+      </p>
 
-            <p>
-                A primeira pessoa que eu pensei foi em você, porque
-                eu sempre quero te mostrar as coisas que aprendi, consegui,
-                conquistei e como estou evoluindo, porque no fim toda a evolução
-                que eu busco e tudo que eu faço é tentando melhorar a nossa vida
-                e a vida da nossa família.
-            </p>
+      <p>
+        A primeira pessoa que eu pensei foi em você, porque eu sempre quero te
+        mostrar as coisas que aprendi, consegui, conquistei e como estou
+        evoluindo, porque no fim toda a evolução que eu busco e tudo que eu faço
+        é tentando melhorar a nossa vida e a vida da nossa família.
+      </p>
 
-            <p>
-                Amo vocês S2
-            </p>
-        </div>
-    )
+      <p>Amo vocês S2</p>
+    </div>
+  );
 }
 
 export default Home;
